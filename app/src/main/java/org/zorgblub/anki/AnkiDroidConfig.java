bdc4ae14616b96253ca96e813ab50a74032a6334
huh88 <62543943+huh88@users.noreply.github.com>
2021-11-17 12:52:44 +0100

    fixed anki template

--- app/src/main/java/org/zorgblub/anki/AnkiDroidConfig.java
+++ app/src/main/java/org/zorgblub/anki/AnkiDroidConfig.java
@@ -11,28 +11,31 @@ public final class AnkiDroidConfig {
     public static final String[] CARD_NAMES = {"Reading Vocab"};
     // CSS to share between all the cards (optional). User will need to install the NotoSans font by themselves
     public static final String CSS = ".card {\n" +
-            " font-family: NotoSansJP;\n" +
-            " font-size: 24px;\n" +
+            " font-size: 23px\n" +
             " text-align: center;\n" +
             " color: black;\n" +
-            " background-color: white;\n" +
-            " word-wrap: break-word;\n" +
+            " background-color: #FFFAF0;\n" +
+            " font-family: yuumichou;\n" +
             "}\n" +
-            "@font-face { font-family: \"NotoSansJP\"; src: url('_NotoSansJP-Regular.otf'); }\n" +
-            "@font-face { font-family: \"NotoSansJP\"; src: url('_NotoSansJP-Bold.otf'); font-weight: bold; }\n" +
+            "@font-face {\n" +
+            "font-family: yuumichou;\n" +
+            "src: url(_yumin.ttf);\n" +
+            "}\n" +
             "\n" +
-            ".big { font-size: 64px; }\n" +
-            ".emph { font-weight: \"bold\"; }\n" +
-            ".small { font-size: 18px;}\n";
+            ".big { font-size: 42px; }\n" +
+            ".small { font-size: 18px;}\n" +
+            "\n" +
+            ".emph {\n" +
+            "font-weight: 600;\n" +
+            "background-color: #FFFF66;\n" +
+            "}\n";
     // Template for the question of each card
-    static final String QFMT1 = "<div class=big>{{Expression}}</div><br>{{Sentence}}";
+    static final String QFMT1 = "<div class=big>{{Word}}</div>";
     public static final String[] QFMT = {QFMT1};
     // Template for the answer (use identical for both sides)
-    static final String AFMT1 = "{{Meaning}}<br><br>\n" +
-            "<div class=small>\n" +
-            "{{Expression}} ({{Reading}})<br><br>\n" +
-            "{{#Reason}}Deinflection: {{Deinflected}}{{Reason}}{{/Reason}}\n" +
-            "</div>";
+    static final String AFMT1 = "{{Sentence}}<br><br><br>\n" +
+            "{{Meaning}}<br><br>\n" +
+            "{{Audio}}\n";
     public static final String[] AFMT = {AFMT1};
     // Define two keys which will be used when using legacy ACTION_SEND intent
     public static final String FRONT_SIDE_KEY = FIELDS[0];
